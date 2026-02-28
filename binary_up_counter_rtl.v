da7466780a86ff5502c589cb4da2a4fc61618984
Shaik-Ishrath <167004394+Shaik-Ishrath@users.noreply.github.com>
2026-02-28 18:05:53 +0530

    Implement binary up counter module

---
+++ binary_up_counter_rtl.v
@@ -0,0 +1,13 @@
+module binary_up_counter(input [3:0]data_in,
+                        input load,clk,resetn,
+                         output reg [3:0]count);
+  always@(posedge clk)
+    begin
+      if(~resetn)
+        count <= 4'd0;
+      else if(load)
+        count <= data_in;
+      else
+        count <= count +1'b1;
+    end
+endmodule
